44d6db153ebb77a231395de8fd89a0d14246bef9
Aman Mittal <amanmittalma@gmail.com>
2025-08-05 12:56:13 +0530

    NPX

---
+++ Namaste_React/Intro/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "chil2" }, [
+    React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
+  ]),
+]);
+
+root.render(parent);
+console.log("Hello");
