8ea73a35c904a2947be462e94536a897ddac2a1d
Aman Mittal <amanmittalma@gmail.com>
2025-08-05 23:06:11 +0530

    Episode 2

--- Namaste_React/app.js
+++ Namaste_React/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
@@ -7,13 +7,13 @@ root.render(heading);
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
+    React.createElement("h2", { id: "heading2" }, "I am h1 tag"),
   ]),
   React.createElement("div", { id: "chil2" }, [
-    React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
+    React.createElement("h1", { id: "heading1" }, "I am hello tag"),
+    React.createElement("h2", { id: "heading2" }, "I am hello tag"),
   ]),
 ]);
 
 root.render(parent);
-console.log("Hello");
+console.log("hello");
